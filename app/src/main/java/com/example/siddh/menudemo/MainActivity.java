93adf9cf378f3b3645520e2a45b27fcf58a8a4bc
siddharthchd <siddharth.chaudhary1705@gmail.com>
2017-07-29 02:22:56 +0530

    Storage using Shared Preferences

---
+++ app/src/main/java/com/example/siddh/menudemo/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.siddh.menudemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.settings :
+                Toast.makeText(this, "Menu item selected : Settings", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.help :
+                Toast.makeText(this, "Menu item selected : Help", Toast.LENGTH_SHORT).show();
+                return true;
+            default :
+                return false;
+
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
